f14e8f3a912d65b8b31788cda4a9e734872a64de
emreturgutce <emrebursalee@gmail.com>
2020-07-14 18:40:12 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useContext, useState } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import NotFound from "./components/NotFound";
+import PrivateRoute from "./components/PrivateRoute";
+import Await from "./components/Await";
+import Axios from "axios";
+import UserContext from "./context/UserContext";
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const { setUser, isLoggedIn } = useContext(UserContext);
+  let time = Date.now;
+
+  const getUser = async () => {
+    try {
+      const token = document.cookie.split("=")[1];
+      if (!token) throw new Error();
+      const response = await Axios.get("http://localhost:5000/me", {
+        headers: { Authorization: `bearer ${token}` },
+      });
+      const data = response.data;
+      if (!data.success) throw new Error();
+      const user = Object.assign({}, data);
+      delete user.createdAt;
+      delete user.updatedAt;
+      setUser(user);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <Await />
+      ) : (
+        <>
+          <h1>Hello World</h1>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link>
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <PrivateRoute exatch path="/" component={Home} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
