54e6ef5df99ef822d7ae73b260efb5844c400c82
emreturgutce <emrebursalee@gmail.com>
2020-07-15 16:18:20 +0300

    unnecessary things deleted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext, useState } from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Login from "./components/Login";
@@ -12,12 +12,16 @@ import UserContext from "./context/UserContext";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-  const { setUser, isLoggedIn } = useContext(UserContext);
-  let time = Date.now;
+  const { setUser } = useContext(UserContext);
 
   const getUser = async () => {
     try {
-      const token = document.cookie.split("=")[1];
+      let token;
+      document.cookie.split("; ").forEach((cookie) => {
+        if (cookie.startsWith("token")) {
+          token = cookie.split("=")[1];
+        }
+      });
       if (!token) throw new Error();
       const response = await Axios.get("http://localhost:5000/me", {
         headers: { Authorization: `bearer ${token}` },
@@ -41,13 +45,11 @@ const App = () => {
   return (
     <div className="App">
       {loading ? (
-        <Await />
+        <div className="loading-container">
+          <Await />
+        </div>
       ) : (
         <>
-          <h1>Hello World</h1>
-          <Link to="/">Home</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
           <Switch>
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
